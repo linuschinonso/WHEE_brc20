e529a3860511b870bf62f346f5e0f6b30d4424d0
Agu C. Christopher <linusaziegwu@gmail.com>
2023-11-29 22:47:51 +0100

    Update

--- app/page.js
+++ app/page.js
@@ -1,7 +1,10 @@
+"use client";
 import Image from "next/image";
 import { Card, Text, Metric } from "@tremor/react";
 import Link from "next/link";
+import { useState } from "react";
 export default function Home() {
+  const [nav, setNav] = useState(true);
   return (
     <div className="bg-[#ffffffdc] dark" data-mode="dark">
       <nav class="bg-dark dark:bg-gray-900  w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
@@ -34,11 +37,12 @@ export default function Home() {
               class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
               aria-controls="navbar-sticky"
               aria-expanded="false"
+              onClick={() => setNav(!nav)}
             >
               <span class="sr-only">Open main menu</span>
               <svg
                 class="w-5 h-5"
-                aria-hidden="true"
+                aria-hidden="false"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 17 14"
@@ -54,7 +58,9 @@ export default function Home() {
             </button>
           </div>
           <div
-            class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+            class={`items-center justify-between ${
+              nav ? "hidden" : ""
+            } w-full md:flex md:w-auto md:order-1`}
             id="navbar-sticky"
           >
             <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
