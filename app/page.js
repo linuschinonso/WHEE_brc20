a3614160b4bb83bb3ab7145b2fff84c416369a29
Agu C. Christopher <linusaziegwu@gmail.com>
2023-11-30 21:09:35 +0100

    Update

--- app/page.js
+++ app/page.js
@@ -148,7 +148,7 @@ export default function Home() {
                   src="./wheebanner.jpg"
                 />
               </div>
-              <div className="flex col-auto justify-center gap-x-2 gap-y-3 mb-5 md:w-[70%] w-[80%]">
+              <div className="flex col-auto justify-center gap-x-2 gap-y-3 mb-5 md:w-[50%] w-[100%]">
                 <p>
                   WHEE Contract ETH: 0x2512c545A0117353e4E9aCDdfF860342640d3CF6
                   WHEE Contract BSC: 0x62238038abeae5a667cde8769e708c5a07d7addb
